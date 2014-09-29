09614f280c2c4467f7caeda52cfb780980a885da
pbfordev <pbfordev@b7cbc083-b4d9-413c-8e36-b7e577f46edc>
2014-09-29 11:15:21 +0000

    Added wxAutoExcelLanguageSettings.{h|cpp} which should be part of r65.

---
+++ trunk/include/wx/wxAutoExcelLanguageSettings.h
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////
+// Author:      PB
+// Modified by:
+// Copyright:   (c) 2012 PB <pbfordev@gmail.com>
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef _WXAUTOEXCEL_LANGUAGESETTINGS_H
+#define _WXAUTOEXCEL_LANGUAGESETTINGS_H
+
+#include "wx/wxAutoExcel_defs.h"
+
+#include "wx/wxAutoExcelObject.h"
+#include "wx/wxAutoExcel_enums.h"
+
+
+namespace wxAutoExcel {
+
+
+    /**
+    @brief Represents Microsoft Excel LanguageSettings object.
+    */
+   class WXDLLIMPEXP_WXAUTOEXCEL wxExcelLanguageSettings : public wxExcelObject
+    {
+    public:        
+
+    // ***** PROPERTIES *****
+
+    /**
+    Gets a MsoAppLanguageID constant representing the locale identifier (LCID) for the install language, the user interface language, or the Help language.
+
+    [MSDN documentation for LanguageSettings.LanguageID](http://msdn.microsoft.com/en-us/library/office/ff863438(v=office.14).aspx).
+    */
+    WXLCID GetLanguageID(MsoAppLanguageID id);
+
+    /**
+    Gets True if the value for the MsoLanguageID constant has been identified in the Windows registry as a preferred language for editing.
+
+    [MSDN documentation for LanguageSettings.LanguagePreferredForEditing](http://msdn.microsoft.com/en-us/library/office/ff861143(v=office.14).aspx).
+    */
+    bool GetLanguagePreferredForEditing(MsoLanguageID lid);
+
+        /**
+        Returns "LanguageSettings".
+        */
+        virtual wxString GetAutoExcelObjectName_() const { return wxS("LanguageSettings"); }
+    };
+
+
+} // namespace wxAutoExcel
+
+#endif //_WXAUTOEXCEL_LANGUAGESETTINGS_H
---
+++ trunk/src/wxAutoExcelLanguageSettings.cpp
@@ -0,0 +1,33 @@
+/////////////////////////////////////////////////////////////////////////////
+// Author:      PB
+// Modified by:
+// Copyright:   (c) 2012 PB <pbfordev@gmail.com>
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+
+#include "wx/wxAutoExcel_prec.h"
+
+#include "wx/wxAutoExcelLanguageSettings.h"
+
+#include "wx/wxAutoExcel_enums.h"
+
+#include "wx/wxAutoExcelPrivate.h"
+
+namespace wxAutoExcel {
+
+// ***** class wxExcelLanguageSettings PROPERTIES *****
+
+WXLCID wxExcelLanguageSettings::GetLanguageID(MsoAppLanguageID id)
+{
+    WXAUTOEXCEL_PROPERTY_LONG_GET1("LanguageID", (long)id);
+}
+
+bool wxExcelLanguageSettings::GetLanguagePreferredForEditing(MsoLanguageID lid)
+{
+    WXAUTOEXCEL_PROPERTY_BOOL_GET1("LanguagePreferredForEditing", (long)lid);
+}
+
+
+
+} // namespace wxAutoExcel
