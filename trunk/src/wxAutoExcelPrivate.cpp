2488fcd5aee59248f7b6a0d369e7970ab691fc42
pbfordev <pbfordev@b7cbc083-b4d9-413c-8e36-b7e577f46edc>
2015-02-15 15:41:11 +0000

    No real change, just refactored LogVariant() a bit

--- trunk/src/wxAutoExcelPrivate.cpp
+++ trunk/src/wxAutoExcelPrivate.cpp
@@ -26,12 +26,12 @@ void LogVariant(const wxString& prefix, const wxVariant& v)
     const wxString type = v.GetType();
 
     wxString info;
-    wxString name = v.GetName();
+    const wxString& name = v.GetName();
     if (type == wxS("arrstring")) {
         wxArrayString as = v.GetArrayString();
         info.Printf(wxS("%svariant type: \"%s\", element count: %zu, name: \"%s\"."),
-            prefix.c_str(), type.c_str(), as.size(), name.c_str());        
-        wxLogTrace(wxTRACE_AutoExcel, wxS("%s"), info.c_str());
+            prefix, type, as.size(), name);        
+        wxLogTrace(wxTRACE_AutoExcel, wxS("%s"), info);
         for (size_t i = 0; i < as.size(); i++) 
         {
             info.Printf(wxS("   string #%zu value: \"%s\""), i, as[i]);
@@ -41,14 +41,14 @@ void LogVariant(const wxString& prefix, const wxVariant& v)
                 break;
             }
             else            
-                wxLogTrace(wxTRACE_AutoExcel, wxS("%s"), info.c_str());
+                wxLogTrace(wxTRACE_AutoExcel, wxS("%s"), info);
         }
         return;
     }
     if (type == wxS("list")) {
         info.Printf(wxS("%sVariant type: \"%s\", element count: %zu, name: \"%s\"."),
-            prefix.c_str(), type.c_str(), v.GetCount(), name.c_str());
-        wxLogTrace(wxTRACE_AutoExcel, wxS("%s"), info.c_str());
+            prefix, type, v.GetCount(), name);
+        wxLogTrace(wxTRACE_AutoExcel, wxS("%s"), info);
         for (size_t i = 0; i < v.GetCount(); i++)
         {
             if ( i == LogVariantMaxItemsInList )
@@ -59,8 +59,8 @@ void LogVariant(const wxString& prefix, const wxVariant& v)
             {
                 const wxVariant& vTmp = v[i];
                 info.Printf(wxS("   variant #%zu type: \"%s\", value: \"%s\", name: \"%s\"."),
-                    i, vTmp.GetType().c_str(), vTmp.MakeString().c_str(), vTmp.GetName().c_str());        
-                wxLogTrace(wxTRACE_AutoExcel, wxS("%s"), info.c_str());
+                    i, vTmp.GetType(), vTmp.MakeString(), vTmp.GetName());        
+                wxLogTrace(wxTRACE_AutoExcel, wxS("%s"), info);
             }
         }
         return;
@@ -72,12 +72,12 @@ void LogVariant(const wxString& prefix, const wxVariant& v)
         dispatch->AddRef();
         object.GetAutomationObject_()->SetDispatchPtr(dispatch);
         info.Printf(wxS("%svariant type: \"IDispatch - %s\", value: \"%s\", name: \"%s\"."),
-            prefix.c_str(), object.GetAutomationObjectName_(false).c_str(), v.MakeString().c_str(), name.c_str());    
+            prefix, object.GetAutomationObjectName_(false), v.MakeString(), name);    
     } else {
         info.Printf(wxS("%svariant type: \"%s\", value: \"%s\", name: \"%s\"."),
-            prefix.c_str(), type.c_str(), v.MakeString().c_str(), name.c_str());        
+            prefix, type, v.MakeString(), name);        
     }
-    wxLogTrace(wxTRACE_AutoExcel, wxS("%s"), info.c_str());
+    wxLogTrace(wxTRACE_AutoExcel, wxS("%s"), info);
 }
 
 
