0946cb5944a61de12c7a0a5c78b32c6589d1e6d7
pbfordev <pbfordev@gmail.com>
2016-07-12 13:52:19 +0200

    No real change, only updated the documentation.

--- docs/doxygen/mainpage.h
+++ docs/doxygen/mainpage.h
@@ -2,30 +2,15 @@
  *
  * @section intro_sec Introduction
  *
- * This is the introduction.
- *
+ * %wxAutoExcel is a C++ library which aims to make automating Microsoft Excel less painful. It requires wxWidgets 3.1 or newer and works only on Microsft Windows.
+ 
  * @section install_sec Installation
-  * @subsection step1 Download and decompress
- Decompress the .zip file into any directory while preserving the folder hierarchy. You should avoid using directories with spaces in their names.
-
- * @subsection step2 Building wxAutoExcel
-  %wxAutoExcel comes with MSVC 2008 and Code::Blocks (using GCC) project files. Go to the folder "wxAutoExcel\build" and you will find wxAutoExcel_vc9.sln for 
- MSVC and wxAutoExcel_gcc.cbp for Code::Blocks there.
- Open the project file of your choice and build one of more of its configurations. There are four of them: Debug, Release, DLL Debug and DLL Release. 
- Verify the build succeeded and the libraries were produced in the %wxAutoExcel\\lib\\ folder. In order to successfully compile %wxAutoExcel with provided project files,
- it is assumed that you have set a system environment variable WXWIN, pointing to the folder where you have installed wxWidxets, e.g. WXWIN=c:\\wxWidgets.
-
- * @subsection step3 Adding wxAutoExcel to your project
-
-This applies to Microsoft Visual C++, but the procedure should be similar in any other  IDE. You need to do the following for all your configurations (e.g. Debug, Release...):
-@li Go to  your project's configuration properties.
-@li In "C/C++ / General" add %wxAutoExcel's include directory into "Additional include directories" (e.g. "c:\wxAutoExcel\include").
-@li In "Linker / General" add %wxAutoExcel's library directory into "Additional library directories" (e.g. "c:\wxAutoExcel\\lib\vc_lib").
-@li In "Linker / Input" add %wxAutoExcel's library into "Additional dependencies" (%wxAutoExcel100ud.lib for the Debug configuration and %wxAutoExcel100u.lib for the Release one).
-@li Optionally add "wx/wxAutoExcel.h" to your precompiled header file to speed up compilation.
-
-Include <wx/%wxAutoExcel.h> in files referring to any %wxAutoExcel class, don't forget that all these classes are in wxAutoExcel namespace.
-
+ * See <a href='https://github.com/pbfordev/wxAutoExcel/blob/master/docs/install.txt'>docs/install.txt</a> for information on how to build and use %wxAutoExcel.
+  
+  * @section FAQ_sec Frequently Asked Questions
+  * See <a href='https://github.com/pbfordev/wxAutoExcel/blob/master/docs/FAQ.txt'>docs/FAQ.txt</a> for the list of answers to the most common questions regarding %wxAutoExcel.
+  
+  
  *  
  * 
  */
