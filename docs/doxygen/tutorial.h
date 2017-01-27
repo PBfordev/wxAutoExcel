09a283924589f14b9fccbaf3b6ebcf266da6146c
pbfordev <PBforDev@gmail.com>
2017-01-27 20:53:47 +0100

    Updated various pieces of documentation.

--- docs/doxygen/tutorial.h
+++ docs/doxygen/tutorial.h
@@ -53,23 +53,35 @@ is more than one instance running, you cannot affect which one you get.
     }    
 @endcode
 
-<b>Showing Excel window/b>
+<b>Showing Excel window</b>
 
 MS Excel launched in automation mode has its window hidden, so you need
 to tell it to show itself if needed.
 @code 
-	// app is a valid instance of wxExcelApplication
+  // app is a valid instance of wxExcelApplication
   app.SetVisible(true);
 @endcode
 
 <b>Closing the application</b>
 
 Once you are done with the application, do not want to use any
-of its objects and wish to close it call
+of its objects and wish to close it.
 @code 
- 	app.Quit();	
+ 	 // app is a valid instance of wxExcelApplication
+   app.Quit();	
 @endcode
 
+
+@section page_tutorial_localised Working with localised Microsoft Excel
+If you want to use wxAutoExcel with Excel localised into languages other 
+than English, I recommend setting its automation LCID to US English, e.g. 
+@code 
+ 	   app.SetAutomationLCID_(wxExcelObject::lcidEnglishUS);
+@endcode
+and you should be able to use English names of formulas and styles etc. 
+regardless of the Excel user language. The LCID will be inherited by all 
+"children" of the object. This unfortunately does not work in all cases 
+and can have some side effects, see the matching entry in docs\FAQ.txt.
 
 @section page_tutorial_workbook Working with workbooks
 <b>Adding a new workbook</b>
@@ -155,8 +167,7 @@ See the bundled samples for more complex examples of obtaining ranges.
     wxExcelRange range = sheet.GetRange("B2:C10");
     
     // range2 will be a cell with an absolute address of "B2"
-    wxExcelRange range2 = range.GetRange("A1");
-    
+    wxExcelRange range2 = range.GetRange("A1");    
     
     wxExcelRange usedRange = sheet.GetUsedRange(); 
 @endcode
@@ -165,7 +176,7 @@ See the bundled samples for more complex examples of obtaining ranges.
 
 See the bundled samples for more complex examples.
 See the bulkadata sample for an example on how to efficiently
-transfer large number of values, using wxSafeArray.
+transfer a large number of values, using wxSafeArray.
 @code         
     // sheet is a valid wxExcelWorksheet instance              
     wxExcelRange range = sheet.GetRange("A1");
@@ -184,7 +195,60 @@ transfer large number of values, using wxSafeArray.
     
     range.SetHorizontalAlignment(xlCenter);
     range.GetInterior().SetColor(*wxLIGHT_GREY);        
-    range.BorderAround(WXAEEP(xlDouble), WXAEEP(xlThick), NULL, wxBLUE);             
+    range.BorderAround(WXAEEP(xlDouble), WXAEEP(xlThick), NULL, wxBLUE);
+    range.GetBorders()[xlInsideHorizontal].SetWeight(xlThin);            
+@endcode
+
+@section page_tutorial_chart Working with charts
+
+Check the bundled charts sample for more examples.
+
+<b>Adding an embedded chart</b>
+@code  
+    wxExcelChart chart;
+    
+    chart = worksheet.GetShapes().AddChart(xlXYScatterLinesNoMarkers, 1, 1, 250, 250).GetChart();
+    chart.SetHasTitle(true);
+    chart.GetChartTitle().SetText("My chart");        
+    chart.SetPlotBy(xlColumns);
+    chart.SetSourceData(sourceRange);                      
+@endcode
+
+<b>Adding a chart sheet </b>
+@code
+    wxExcelChart chart = workbook.GetCharts().Add();    
+    chart.SetChartType(xl3DColumnClustered);
+    chart.SetName("My 3D clustered chart");
+@endcode
+
+<b>Customising chart elements</b>
+@code
+    // chart is of xlXYScatterLinesNoMarkers type
+     
+    // customise the y axis
+    wxExcelAxis axis = chart.Axes(xlValue);
+    
+    axis.SetHasTitle(true);
+    axis.GetAxisTitle().SetCaption(_("Some values"));
+    axis.SetMajorTickMark(xlTickMarkCross);    
+    axis.GetTickLabels().GetFont().SetSize(8);
+    axis.SetMinimumScale(0);
+    axis.SetMaximumScale(200);
+    
+    // customise series
+    wxExcelSeries series = chart.SeriesCollection()[1];  
+    
+    series.SetName(_("My series 1"));
+    series.GetFormat().GetLine().SetWeight(1.5);
+    series.GetFormat().GetLine().GetForeColor().SetRGB(*wxBLUE);
+    
+    // customise individual point appearance
+    wxExcelPoints points = series.Points();
+    wxExcelPoint point = series[1]; // first point
+
+    point.SetMarkerForegroundColor(*wxRED);
+    point.SetMarkerSize(7);
+    point.SetMarkerStyle(xlMarkerStylePlus);                  
 @endcode
 
  */
