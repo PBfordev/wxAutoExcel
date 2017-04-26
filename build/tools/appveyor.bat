69c177908d2b27764568d11818baf1f9e26ea3f6
pbfordev <PBforDev@gmail.com>
2017-04-26 19:30:45 +0200

    No real change, just enhanced appveyor building process, now builds both with MinGW and MSVC.

---
+++ build/tools/appveyor.bat
@@ -0,0 +1,29 @@
+md %build_dir% 
+cd %build_dir%
+
+echo wxWidgets_CONFIGURATION:STRING=mswu > CMakeCache.txt
+echo wxWidgets_LIB_DIR:PATH=%wxWidgets_LIBRARIES% >> CMakeCache.txt
+echo wxWidgets_ROOT_DIR:PATH=%wxWidgets_ROOT_DIR% >> CMakeCache.txt
+echo WX_LIB_DIR:INTERNAL=%wxWidgets_LIBRARIES% >> CMakeCache.txt
+echo WX_ROOT_DIR:INTERNAL=%wxWidgets_ROOT_DIR% >> CMakeCache.txt
+echo wxAutoExcel_BUILD_SHARED:BOOL=OFF >> CMakeCache.txt 
+echo wxAutoExcel_BUILD_LINK_WX_SHARED:BOOL=ON >> CMakeCache.txt    
+
+goto %toolset%
+
+:msbuild
+rem "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+CALL "C:\Program Files (x86)\Microsoft Visual Studio %VisualStudioVersion%\VC\vcvarsall.bat" %platform%
+cmake -Wno-dev -G "Visual Studio 14 2015 Win64" %project_dir%
+msbuild "ALL_BUILD.vcxproj" /consoleloggerparameters:Verbosity=minimal /target:Build  /p:Configuration=%configuration% /p:Platform=%platform% /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
+goto :eof
+
+:mingw
+echo wxAutoExcel_BUILD_GCC_STD_OPTION:STRING=gnu++ >> CMakeCache.txt
+echo wxAutoExcel_BUILD_CXX_STANDARD:STRING=11 >> CMakeCache.txt
+set path=C:\MinGW\bin;"C:\Program Files (x86)\CMake\bin"
+cmake -Wno-dev -G "MinGW Makefiles" %project_dir%
+mingw32-make -j2 -f makefile
+goto :eof
+
+:eof
