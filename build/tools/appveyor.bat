3400ed65aab2f97604c9ba0d7b209e516bb0dfca
PB <pbfordev@gmail.com>
2018-03-09 23:40:02 +0100

    Update AppVeyor build
    Add MinGW64-w64 GCC 7.2.0 64-bit configuration
    Build wxAutoExcel as a shared instead of static library
    Use official wxWidgets 3.1.1 release

--- build/tools/appveyor.bat
+++ build/tools/appveyor.bat
@@ -1,35 +1,36 @@
 md %build_dir% 
 cd %build_dir%
 
-echo wxWidgets_CONFIGURATION:STRING=mswudll > CMakeCache.txt
+echo CMAKE_BUILD_TYPE:STRING=%configuration% >> CMakeCache.txt
+echo wxWidgets_CONFIGURATION:STRING=mswu > CMakeCache.txt
 echo wxWidgets_LIB_DIR:PATH=%wxWidgets_LIBRARIES% >> CMakeCache.txt
 echo wxWidgets_ROOT_DIR:PATH=%wxWidgets_ROOT_DIR% >> CMakeCache.txt
 echo WX_LIB_DIR:INTERNAL=%wxWidgets_LIBRARIES% >> CMakeCache.txt
 echo WX_ROOT_DIR:INTERNAL=%wxWidgets_ROOT_DIR% >> CMakeCache.txt
-echo wxAutoExcel_BUILD_SHARED:BOOL=OFF >> CMakeCache.txt 
-echo wxAutoExcel_BUILD_LINK_WX_SHARED:BOOL=ON >> CMakeCache.txt    
+echo wxAutoExcel_BUILD_SHARED:BOOL=ON >> CMakeCache.txt 
+echo wxAutoExcel_BUILD_LINK_WX_SHARED:BOOL=ON >> CMakeCache.txt
 
 goto %toolset%
 
 :msbuild
-rem "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
-CALL "C:\Program Files (x86)\Microsoft Visual Studio %VisualStudioVersion%\VC\vcvarsall.bat" %platform%
 cmake -Wno-dev -G "Visual Studio 14 2015 Win64" %project_dir%
 msbuild "ALL_BUILD.vcxproj" /consoleloggerparameters:Verbosity=minimal /target:Build  /p:Configuration=%configuration% /p:Platform=%platform% /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 goto :eof
 
 :nmake
-echo CMAKE_BUILD_TYPE:STRING=%configuration% >> CMakeCache.txt
 CALL "C:\Program Files (x86)\Microsoft Visual Studio %VisualStudioVersion%\VC\vcvarsall.bat" %platform%
 cmake -Wno-dev -G "NMake Makefiles" %project_dir%
 nmake -f makefile
 goto :eof
 
-:mingw
-echo CMAKE_BUILD_TYPE:STRING=%configuration% >> CMakeCache.txt
-echo wxAutoExcel_BUILD_GCC_STD_OPTION:STRING=gnu++ >> CMakeCache.txt
-echo wxAutoExcel_BUILD_CXX_STANDARD:STRING=11 >> CMakeCache.txt
-set path=C:\MinGW\bin;"C:\Program Files (x86)\CMake\bin"
+:gcc530
+set path=C:\MinGW\bin;C:\Program Files (x86)\CMake\bin
+cmake -Wno-dev -G "MinGW Makefiles" %project_dir%
+mingw32-make -j2 -f makefile
+goto :eof
+
+:gcc720_x64
+set path=C:\mingw-w64\x86_64-7.2.0-posix-seh-rt_v5-rev1;C:\Program Files (x86)\CMake\bin
 cmake -Wno-dev -G "MinGW Makefiles" %project_dir%
 mingw32-make -j2 -f makefile
 goto :eof
