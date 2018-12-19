01eddeb76147761a192f32fb9159e91f66a33523
PB <pbfordev@gmail.com>
2018-12-19 21:06:51 +0100

    Fix a typo in the Classes page of doxygen docs

--- docs/doxygen/classes.h
+++ docs/doxygen/classes.h
@@ -8,7 +8,7 @@
 
 @page page_classes List of Supported Microsft Excel Classes 
  
- Please go <a href='annotated.html'>here</a> to see the alphabeticall list 
+ Please go <a href='annotated.html'>here</a> to see the alphabetical list 
  of classes which are supported by %wxAutoExcel. All the classes have the
  same name as the wrapped Microsoft Excel ones, except they are prefixed 
  with "wxExcel". Please do not forget that all %wxAutoExcel classes and
