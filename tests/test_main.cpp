89a6f25afdd7757a6bac7ffed6ea1cd8036fd49f
PB <pbfordev@gmail.com>
2026-09-25 12:50:09 +0200

    Fail early when Excel application creation fails

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -8,6 +8,8 @@
 
 #include <catch2/catch_session.hpp>
 
+#include <iostream>
+
 #include <wx/app.h>
 #include <wx/wxAutoExcel.h>
 
@@ -31,28 +33,47 @@ wxAutoExcel::wxExcelApplication& GetApplication()
 
 } // namespace wxAutoExcelTests
 
-int main(int argc, char* argv[])
+namespace
 {
-    wxApp::SetInstance(new wxApp());
-    if ( !wxEntryStart(argc, argv) )
-        return 1;
 
+int RunTests(int argc, char* argv[])
+{
     wxAutoExcel::wxAutoExcelObjectErrorModeOverrider errorMode{
         wxAutoExcel::wxExcelObject::Err_DoNothing, true
     };
     wxAutoExcel::wxExcelApplication application =
         wxAutoExcel::wxExcelApplication::CreateInstance();
-    wxAutoExcelTests::application = &application;
 
-    if ( application )
-        application.SetDisplayAlerts(false);
+    if ( !application )
+    {
+        std::cerr
+            << "Unable to create a Microsoft Excel application instance.\n"
+            << "Verify that Microsoft Excel is installed and available for "
+               "OLE automation.\n";
+        return 1;
+    }
+
+    application.SetDisplayAlerts(false);
+    wxAutoExcelTests::application = &application;
 
     const int result = Catch::Session().run(argc, argv);
 
     wxAutoExcelTests::application = NULL;
     if ( application.IsOk_() )
         application.Quit();
-    application = wxAutoExcel::wxExcelApplication();
+
+    return result;
+}
+
+} // unnamed namespace
+
+int main(int argc, char* argv[])
+{
+    wxApp::SetInstance(new wxApp());
+    if ( !wxEntryStart(argc, argv) )
+        return 1;
+
+    const int result = RunTests(argc, argv);
 
     wxEntryCleanup();
     return result;
